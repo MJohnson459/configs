7f3c5735fb86cac300b089c819f8d84f0c7b0e5b
Michael Johnson <michael.johnson@seebyte.com>
2018-08-16 14:53:08 +0100

    Installing ssh config and youcompleteme

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+# Install fun
+sudo apt install -y \
+  neovim \
+  curl \
+  fonts-hack-ttf \
+  tmux silversearcher-ag \
+  cmake \
+  python-dev \
+  python3-dev
+
 # vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "source $(pwd)/.vimrc" > ~/.vimrc
 echo "source $(pwd)/.gvimrc" > ~/.gvimrc
+mkdir -p $HOME/.vim/backup $HOME/.vim/undo
+
+# neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # tmux
 echo "source-file $(pwd)/.tmux.conf" > ~/.tmux.conf
 cp .tmuxline ~/.tmuxline
+
+# ssh
+cp .ssh_config ~/.ssh/config
+
+# git
+cp .gitconfig ~/.gitconfig
